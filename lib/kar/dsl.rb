3d61ab084c69df7fe9fcd6cb4caa3b03e6f50fa0
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2025-10-26 04:06:01 +0900

    Add cargo DSL

--- lib/kar/dsl.rb
+++ lib/kar/dsl.rb
@@ -1,4 +1,5 @@
 require "kar/uritask"
+require "kar/cargotask"
 
 module Kar
   # DSLs you can use in your Rakefiles
@@ -18,6 +19,9 @@ module Kar
   #
   #   # Downloads file from https://example.com/remote/file and save to local/file
   #   download "local/file" => "https://example.org/remote/file"
+  #
+  #   # Build extension in Rust
+  #   cargo "my_gem"
   module DSL
     def files(file_list)
       file_list.each do |file_name|
@@ -44,6 +48,27 @@ module Kar
     # @raise [ArgumentError] if URI scheme is unsupported
     def download(args)
       URITask.new args
+    end
+
+    # Declare a cargo task which builds extension in Rust by Cargo.
+    #
+    # @example
+    #   # Rakefile
+    #   cargo "my_gem"
+    #
+    #   task build: :cargo
+    #
+    #   # shell
+    #   % rake cargo
+    #
+    # Also defines a cargo:validate task which validates version `Cargo.lock`s are up-to-date.
+    # It's useful to add them to the dependency of build task and prevent building invalid gem package.
+    #
+    # Uses {https://docs.ruby-lang.org/en/master/Gem/Ext/CargoBuilder.html Gem::Ext::CargoBuilder} internally unlike {https://oxidize-rb.org/docs/api-reference/rb-sys-gem-config#rbsysextensiontask RbSys::ExtensionTask}
+    # in order to build in the same way as `gem install` command, even though the output might be less usefull.
+    # When `RbSys::Extension` uses the same way, this function may be deprecated.
+    def cargo(target)
+      CargoTask.new target
     end
 
     Rake::DSL.prepend self
