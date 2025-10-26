0c8a8578a88bab04f29cc86f90537d12202a10a8
Kitaiti Makoto <KitaitiMakoto@gmail.com>
2025-10-27 05:56:00 +0900

    Remove unnecessary module declaration

--- lib/kar.rb
+++ lib/kar.rb
@@ -1,8 +1,2 @@
-# frozen_string_literal: true
-
-require_relative "kar/version"
-
-module Kar
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require "kar/uritask"
+require "kar/cargotask"
